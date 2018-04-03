72abc4b5153555e9ff521adf991d858b955e1324
Edvard Bakken <edvard.vb@live.no>
2018-04-03 13:54:23 +0200

    Better args

--- classify.py
+++ classify.py
@@ -7,15 +7,16 @@ from constants import header
 from utils import get_training_set, get_input_set, get_stop_words
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-training', help='path to training set', type=str)
-parser.add_argument('-input', help='path to input file', type=str)
-parser.add_argument('-output', help='path to output file', type=str)
-parser.add_argument('-sample', type=bool)
+parser.add_argument('-training', '-t', help='Path to training set', type=str)
+parser.add_argument('-input', '-i', help='Path to input file', type=str)
+parser.add_argument('-output', '-o', help='Path to output file', type=str)
+parser.add_argument('-sample', '-s', help='Used to sample the training set for shorter computation time', action='store_true')
 args = parser.parse_args()
 
 print(args.training)
 print(args.input)
 print(args.output)
+print(args.sample)
 
 def setup():
     conf = SparkConf().setAppName(f'Phase2-{datetime.now()}')
